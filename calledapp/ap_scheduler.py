53029a18d4320fb2467ebe3fc327b315f216a85f
Fujiyuki-Maeda <goku-maeda@outlook.jp>
2024-04-02 21:34:47 +0900

    fivecommit

--- calledapp/ap_scheduler.py
+++ calledapp/ap_scheduler.py
@@ -1,12 +1,16 @@
 from datetime import datetime, date
 from apscheduler.schedulers.background import BackgroundScheduler
 import pyautogui
+import webbrowser
 
 
 def periodic_execution():
     pyautogui.hotkey('ctrl', 'r')
+    print("更新")
     
 def start():
     scheduler = BackgroundScheduler()
     scheduler.add_job(periodic_execution, 'interval', minutes=1)# 1分おきに実行
     scheduler.star
+    
+    webbrowser.open("https://fujiyukimaeda.pythonanywhere.com/index/")
